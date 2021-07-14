e745a26a549b32f6979423dbc6791945466bf2aa
Admin i <ihsanmpm@gmail.com>
2021-07-14 14:43:35 +0530

    inMemoryAuthentication

---
+++ src/main/java/com/ihs2code/springsecurity/dm/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.ihs2code.springsecurity.dm.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
