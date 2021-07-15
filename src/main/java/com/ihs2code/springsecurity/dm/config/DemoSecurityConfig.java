ba3140659eee4bf7b5a48d7de71036293b69844a
Admin i <ihsanmpm@gmail.com>
2021-07-15 11:35:41 +0530

    anyRequest().authenticated()

--- src/main/java/com/ihs2code/springsecurity/dm/config/DemoSecurityConfig.java
+++ src/main/java/com/ihs2code/springsecurity/dm/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.ihs2code.springsecurity.dm.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,6 +24,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("mary").password("test123").roles("MANAGER").and()
 			.withUser("nancy").password("test123").roles("ADMIN");
 			
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
+		
 	}
 
 	
---
+++ src/main/java/com/ihs2code/springsecurity/dm/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.ihs2code.springsecurity.dm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+		return "plain-login";
+	}
+}
+
